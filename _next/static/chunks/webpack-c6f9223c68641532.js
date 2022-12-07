9b1e621a390e96791ef65bc0e8ea6de4bc3d6a14
alaynajasmyn <thinshanty@gmail.com>
2022-12-07 11:45:39 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-c6f9223c68641532.js
+++ _next/static/chunks/webpack-c6f9223c68641532.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "1b45bb3831cc8f62" + ".css";
+/******/ 			return "static/css/" + "36facb09e6163ff7" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
